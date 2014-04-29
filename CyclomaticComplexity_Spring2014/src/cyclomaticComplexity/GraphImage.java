24983f17213403955b213b399c2bfb636f047ed9
Josue Palmerin <josuepalmerin@gmail.com>
2014-04-29 16:30:43 -0500

    graphImage update

--- CyclomaticComplexity_Spring2014/src/cyclomaticComplexity/GraphImage.java
+++ CyclomaticComplexity_Spring2014/src/cyclomaticComplexity/GraphImage.java
@@ -1,12 +1,50 @@
 package cyclomaticComplexity;
 
 import java.awt.Image;
+import java.io.File;
+import java.util.ArrayList;
+
+import javax.swing.JTextArea;
+
+import engine.GraphEngine;
+import engine.GraphEngineImpl;
+import engine.GraphViz;
+import engine.MethodData;
 
 public class GraphImage 
 {
-	public static Image getImage (String body) throws RuntimeException
+	public static File getImage (MethodData body) throws RuntimeException
 	{
-		return null;
+		File img;
+		GraphEngine g = new GraphEngineImpl();
+		GraphModel graph = new GraphModelImpl();
+		graph = g.getGraphModel(body);
+		
+		ArrayList<int[]> ali = graph.getPaths();
+    	ArrayList<String> as = graph.getNodes();
+    	
+		String text = "";
+    	JTextArea label = new JTextArea();
+    	for (int i = 0; i < as.size();i++){
+			text = text + ("("+i+")("+as.get(i)+")")+"\n";
+		}
+    	text = text + "Complexity = " + CyclomaticComplexityMethodCalc.getCyclomaticComplexity(body);
+    	label.setText(text);
+		
+		GraphViz gv = new GraphViz();
+		gv.addln(gv.start_graph());
+		for (int i = 0; i < ali.size();i++){
+			gv.addln(ali.get(i)[0]+" -> "+ali.get(i)[1]+";");
+		}
+		gv.addln(gv.end_graph());
+		
+		String type = "png";
+		File out = new File("out." + type);   // out.gif in this example
+		gv.writeGraphToFile( gv.getGraph( gv.getDotSource(), type ), out );
+		
+		img=out;
+		
+		return img;
 		
 	}
 	 
